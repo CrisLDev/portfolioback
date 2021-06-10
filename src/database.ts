f6718c9b5ac2316551d37215e1017815a1a9dc05
CrisLDev <leonardomoreirapazmio@gmail.com>
2021-06-10 18:54:49 -0500

    add comand and mongo uri

--- src/database.ts
+++ src/database.ts
@@ -1,6 +1,8 @@
 import mongoose, {ConnectionOptions} from 'mongoose';
 import config from './config';
 
+const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost/portfolio';
+
 (async () => {
     try {
         const mongooseOptions: ConnectionOptions = {
@@ -9,7 +11,7 @@ import config from './config';
             // user: config.MONGO_USER,
             // pass: config.MONGO_PASSWORD
         }
-        const db = await mongoose.connect(`mongodb://${config.MONGO_HOST}/${config.MONGO_DATABASE}`, mongooseOptions);
+        const db = await mongoose.connect(MONGO_URI, mongooseOptions);
         console.log('Database is connected to', db.connection.name)
     } catch (err) {
         console.error(err)
